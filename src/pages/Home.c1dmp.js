90859feeac9a64d8e5bd94ea9c51453c4b11fe23
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-06-25 15:41:46 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,8 @@
+// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+
+$w.onReady(function () {
+  echo()
+});
+
+const echo = () => console.log('Hello World!')
